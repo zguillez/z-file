a1d16fe7bca05d0a268a5bfca202e0c5e7f59a57
Guillermo de la Iglesia <zguillez@gmail.com>
2017-01-13 09:51:56 +0100

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # z-file
 Nodejs tools for file streams
+
+## Install
+```
+npm i z-file
+```
+# Usage
+```
+const file = require('z-file');
+```
+
+## Read file
+
+```
+file.read('./src/data.csv').then((data) => {
+  console.log(data);
+});
+```
+
+## Write file
+
+```
+file.write('./src/data.csv', data).then((data) => {
+  console.log(data);
+});
+```
+
+# Use as object
+```
+let f = file.create();
+f.load('./src/data.csv').then(() => {
+  f.data = 'Hello world!;
+  f.save().then(() => {
+    console.log(f.data);
+  });
+});
+```
+
+### Creating new file and load content from existing file
+
+```
+let f = file.create('./src/newfile.csv');
+f.load('./src/oldfile.csv').then(() => {
+  f.data = 'Hello world!;
+  f.save().then(() => {
+    console.log(f.path); //file is saved on './src/newfile.csv'
+  });
+});
+```
+
+## Saving as a new file
+
+```
+let f = file.create();
+f.load('./src/file.csv').then(() => {
+  f.data = 'Hello world!;
+  f.save('./src/newfile.csv').then(() => {
+    console.log(f.path); //file is saved on './src/newfile.csv'
+  });
+});
+```
+
+# Contributing and issues
+Contributors are welcome, please fork and send pull requests! If you have any ideas on how to make this project better then please submit an issue or send me an [email](mailto:mail@zguillez.io).
+
+# License
+©2017 Zguillez.io
+
+Original code licensed under [MIT](https://en.wikipedia.org/wiki/MIT_License) Open Source projects used within this project retain their original licenses.
+
+# Changelog
+
+### v0.1.0 (January 12, 2017)
+* Basic implementation for file
--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 'use strict';
 const fs = require('fs');
+const readfile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf-8', (err, data) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+const writefile = (filename, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filename, data, (err) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+};
 module.exports = {
-  read: (filename) => {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filename, 'utf-8', (err, data) => {
-        if(err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    });
-  },
-  write: (filename, data) => {
-    return new Promise((resolve, reject) => {
-      fs.writeFile(filename, data, (err, data) => {
-        if(err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    });
+  read: readfile,
+  write: writefile,
+  create: (path) => {
+    return new File(path);
   },
 };
+/**
+ *
+ * @constructor
+ */
+function File(path) {
+  this.path = path;
+  this.filename = null;
+  this.data = '';
+  //-------------------------
+  this.load = (filename) => {
+    this.filename = (filename) ? filename : this.path;
+    this.path = (this.path) ? this.path : this.filename;
+    return new Promise((resolve, reject) => {
+      if(! this.filename && ! this.path) {
+        reject(new Error("Null filepath"));
+      } else {
+        readfile(this.filename).then((data) => {
+          this.data = data;
+          resolve(this);
+        });
+      }
+    });
+  };
+  this.save = (path) => {
+    this.path = (path) ? path : this.path;
+    return new Promise((resolve, reject) => {
+      writefile(this.path, this.data).then(() => {
+        resolve(this);
+      });
+    });
+  };
+}
+
