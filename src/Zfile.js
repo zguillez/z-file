942d6c73eace890aa757ff46f6aa415fd14858bb
Guillermo de la Iglesia <zguillez@gmail.com>
2020-04-25 01:25:28 +0200

    update

--- src/Zfile.js
+++ src/Zfile.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const del = require('del');
 const fileType = require('file-type');
 const Jimp = require('jimp');
-const ZfileObject = require('./ZfileObject.js');
 
 /**
  * Class Zfile
@@ -26,7 +25,7 @@ class Zfile {
         if (err) {
           reject(err);
         } else {
-          if (getFileType(data)) {
+          if (fileType(data)) {
             resolve(data);
           } else {
             resolve(data.toString());
@@ -237,6 +236,89 @@ class Zfile {
       });
     });
   }
+}
+
+/** ************************************************** */
+/**
+ * Class ZfileObject
+ * Genera un objecto que puede leer y guardar datos en ficheros
+ */
+class ZfileObject {
+  /**
+   * @param {string} engine
+   * @param {string} path - El nombre del fichero
+   */
+  constructor(engine, path) {
+    /**
+     * Zfile class
+     */
+    this.engine = engine;
+    /**
+     * Ruta del fichero de origen
+     * @type {string}
+     */
+    this.path = path;
+    /**
+     * Nombre del fichero de destino
+     * @type {null}
+     */
+    this.filename = null;
+    /**
+     * Datos del fichero
+     * @type {string}
+     */
+    this.data = '';
+  }
+
+  /**
+   * Lee datos de un fichero
+   * @param {string} filename
+   * @return {Promise}
+   */
+  load(filename) {
+    this.filename = (filename) ? filename : this.path;
+    this.path = (this.path) ? this.path : this.filename;
+    return new Promise((resolve, reject) => {
+      if (!this.filename && !this.path) {
+        reject(new Error('Null filepath'));
+      } else {
+        this.engine.read(this.filename).then((data) => {
+          this.data = data;
+          resolve(this);
+        });
+      }
+    });
+  };
+
+  /**
+   * Guarda datos en un fichero
+   * @param {string} path
+   * @return {Promise}
+   */
+  save(path) {
+    this.path = (path) ? path : this.path;
+    return new Promise((resolve, reject) => {
+      this.engine.write(this.path, this.data).then(() => {
+        resolve(this);
+      });
+    });
+  };
+
+  /**
+   * Obtiene el tipo de datos
+   * @return {string}
+   */
+  getFileType() {
+    let type = fileType(this.data);
+    if (!type) {
+      const arr = this.path.split('.');
+      type = {
+        ext: arr[arr.length - 1],
+        mime: 'text/plain',
+      };
+    }
+    return type;
+  };
 }
 
 /**
