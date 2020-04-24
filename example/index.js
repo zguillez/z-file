77b553c8dcf14e3bca4b81f7072148c8c5f645e8
Guillermo de la Iglesia <zguillez@gmail.com>
2020-04-25 00:22:52 +0200

    update

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const zfile = require('../src/Zfile.js');
-zfile.checkIfFolderExists('.temp', true);
+zfile.folder('.temp', true);
 // let f = zfile.create('.temp/test.txt');
 // f.load('bin/test.js').then(() => f.save());
 // let f = zfile.checkIfFileExists('.temp/test.txt', true);
@@ -12,6 +12,6 @@ zfile.checkIfFolderExists('.temp', true);
 /* zfile.psdToJpg('300x250.psd', '300x250.jpg', 80)
     .then(() => console.log('ok'))
     .catch((err) => console.log(err));*/
-// zfile.dummy(300, 250, '#FF2200', 'dummy.jpg');
+zfile.dummy(300, 250, '#ff2200', '.temp/dummy.jpg');
 // zfile.dummy(300, 250, '#ffb65b', 'dummy.gif');
 console.log('done!');
